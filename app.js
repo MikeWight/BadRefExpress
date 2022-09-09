6daf576b34f93ac8204c0a609f9047e8f32a4eb9
MikeWight <michel.wight@hotmail.com>
2022-09-08 21:04:15 -0300

    added a custom user call

--- app.js
+++ app.js
@@ -1,29 +1,15 @@
 const express = require('express')
+const users = require('./users')
 const app = express()
 
-app.use(loggingMiddleware)
 
 app.get('/', (req, res) => {
-  res.send('Home Page')
+  res.json(users);
 })
 
-app.get('/users', authorizeUsersAccess, (req, res) => {
+app.get('/users', (req, res) => {
   console.log(req.admin)
   res.send('Users Page')
 })
-
-function loggingMiddleware(req, res, next) {
-  console.log(`${new Date().toISOString()}: ${req.originalUrl}`)
-  next()
-}
-
-function authorizeUsersAccess(req, res, next) {
-  if (req.query.admin === 'true') {
-    req.admin = true
-    next()
-  } else {
-    res.send('ERROR: You must be an admin')
-  }
-}
 
 app.listen(3001, () => console.log('Server Started'))
